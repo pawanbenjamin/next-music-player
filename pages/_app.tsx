0f5ef88fd0bda20c0894cf427e3c379a52fdab1d
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-11-16 12:57:40 -0600

    date time removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,26 @@
-import "../styles/globals.css"
-import type { AppProps } from "next/app"
-import "reset-css"
-import { ChakraProvider } from "@chakra-ui/react"
-import PlayerLayout from "../components/PlayerLayout"
-import { useRouter } from "next/router"
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "reset-css";
+import { ChakraProvider } from "@chakra-ui/react";
+import PlayerLayout from "../components/PlayerLayout";
+import { StoreProvider } from "easy-peasy";
+import { store } from "../lib/store";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <ChakraProvider>
-      {router.pathname === "/signin" || router.pathname === "/signup" ? (
-        <Component {...pageProps} />
-      ) : (
-        <PlayerLayout>
+      <StoreProvider store={store}>
+        {router.pathname === "/signin" || router.pathname === "/signup" ? (
           <Component {...pageProps} />
-        </PlayerLayout>
-      )}
+        ) : (
+          <PlayerLayout>
+            <Component {...pageProps} />
+          </PlayerLayout>
+        )}
+      </StoreProvider>
     </ChakraProvider>
-  )
+  );
 }
